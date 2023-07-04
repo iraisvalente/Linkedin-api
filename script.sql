c490ad4672df6fba7beb2c21ab4cb2c6c8521026
iraisvalente <ivalente@soaprojects.com>
2023-07-04 10:34:14 -0600

    adding sql tables in script

--- script.sql
+++ script.sql
@@ -19,3 +19,21 @@ CREATE TABLE Connections(
     Connection varchar(255),
     Upload_Date datetime default current_timestamp
 );
+
+CREATE TABLE Positions(
+	Id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	Position varchar(255)
+);
+
+CREATE TABLE Searches(
+	Id INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(255),
+    Note VARCHAR(255),
+    Search BOOLEAN,
+    Connection_first_name VARCHAR(255),
+    Connection_last_name VARCHAR(255),
+    Connection_email VARCHAR(255),
+    Connection_company VARCHAR(255),
+    Connection_position VARCHAR(255),
+    Connection_connection VARCHAR(255)
+);
